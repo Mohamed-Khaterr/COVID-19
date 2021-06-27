44a172bc6ce3838bf162f0fda81d399a42f6a540
Mohamed Khater <mohamed.15751@gmail.com>
2021-06-27 07:27:19 -0700

    Initial Commit

---
+++ COVID-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID-19
+//
+//  Created by Khater on 6/27/21.
+//  Copyright © 2021 Khater. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
